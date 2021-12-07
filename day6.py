34b30708aa7428a2ff69e4955d823743b7c49490
Pavlo Ivashkov <pavels.code@gmail.com>
2021-12-07 17:15:36 +0200

    day 6

--- day6.py
+++ day6.py
@@ -36,6 +36,21 @@ print(part1(data))
 print(part2(data))
 
 
+def npsolve(data, N):
+    import numpy as np
+    xs = list(map(int, data.split(',')))
+    x = np.zeros((9,), dtype=np.int64)
+    for n in xs:
+        x[n] += 1
+    a = np.zeros((9,9), dtype=np.int64)
+    for i in range(8):
+        a[i,i+1] = 1
+    a[6,0] = 1
+    a[8,0] = 1
+    a = np.linalg.matrix_power(a, N)
+    return np.sum(a * x)
+
+
 def analysis(data):
     Ns = list(range(0, 51, 1))
     print('N =', ' '.join(map(str, Ns)))
