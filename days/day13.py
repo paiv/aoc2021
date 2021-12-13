7c01e06a4f46758ec2ae77efe9a9cd7fb8fd75a9
Pavlo Ivashkov <pavels.code@gmail.com>
2021-12-14 00:39:55 +0200

    :space_invader:

--- days/day13.py
+++ days/day13.py
@@ -86,3 +86,41 @@ assert part2(data) == 0
 data = open('day13.in').read()
 print(part1(data))
 print(part2(data))
+
+
+def generate(image, N=10):
+    import random
+    rr = random.randrange
+    dots = [(x,y) for y,s in enumerate(image.strip().splitlines())
+        for x,c in enumerate(s) if c == '#']
+    minx = min(x for x,y in dots)
+    maxx = max(x for x,y in dots)
+    miny = min(y for x,y in dots)
+    maxy = max(y for x,y in dots)
+    ops = list()
+    for i in range(N):
+        if (i > 1) and (i % 2):
+            q = maxx + 1
+            ops.append(('x', q))
+            dots = [(2 * q - x, y) if rr(2) else (x,y) for x,y in dots]
+            maxx = 2 * q - minx
+        else:
+            q = maxy + 1
+            ops.append(('y', q))
+            dots = [(x, 2 * q - y) if rr(2) else (x,y) for x,y in dots]
+            maxy = 2 * q - miny
+    sa = '\n'.join(','.join(map(str, p)) for p in dots)
+    sb = '\n'.join('fold along ' + '='.join(map(str, p)) for p in reversed(ops))
+    print(sa + '\n\n' + sb)
+
+
+# generate('''
+# ..#.....#.....#.....#.....#.....#..
+# ...#...#.......#...#.......#...#...
+# ..#######.....#######.....#######..
+# .##.###.##...##.###.##...##.###.##.
+# ###########.###########.###########
+# #.#######.#.#.#######.#.#.#######.#
+# #.#.....#.#.#.#.....#.#.#.#.....#.#
+# ...##.##.......##.##.......##.##...
+# ''')
